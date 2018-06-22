ecb9ff47c2fc40112ddec35dab1c28021bee6181
wagnerjrcampos <wagnerjrcampos26@gmail.com>
2018-06-21 21:12:20 -0300

    inicio projeto php

---
+++ exercicio.php
@@ -0,0 +1,18 @@
+<?php
+
+	$titulo = "Título da página";
+	$tituloconteudo = "<h1>titulo do texto";
+	$conteudo = "<p>GIT - versionamento</p>"
+?>
+
+<html>
+<head>
+		<title><?php echo $titulo; ?></title>
+		<meta charset= "UTF-8" />
+</head>
+<body>
+<?php
+	echo $tituloconteudo
+	echo $conteudo;
+	?>
+</body>	
